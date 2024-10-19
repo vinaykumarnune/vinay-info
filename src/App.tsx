b80d1c0dba5b30a8b5b78c2d847da43725e84c4d
vinay kumar <vinnu3005@gmail.com>
2024-10-20 00:59:00 +0530

    added try catch for pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
 import React from "react";
-import {  Routes, Route, Navigate } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
 import Homepage from "./pages/Homepage";
 import Blog from "./pages/Blog";
 import SingleBlog from "./pages/SingleBlog";
 function App() {
   return (
-    
+    <Router>
       <Routes>
         <Route path="/home" element={<Homepage />} />
         <Route path="/" element={<Navigate replace to="/home" />} />
@@ -13,7 +13,7 @@ function App() {
         <Route  path="/category/:categoryid/article/:articleid" element={<SingleBlog />} />
         <Route  path="/article/:articleid" element={<SingleBlog />} />
       </Routes>
-    
+    </Router>
   );
 }
 
